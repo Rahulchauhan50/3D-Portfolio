03f0be5ccb81ec6e0b3721f15852f81a29f73836
Rahul chauhan <rahulchauhah50@gmail.com>
2023-08-17 17:03:45 +0530

    complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,6 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant} from '../utils/motion'
 import { SectionWrapper } from '../hoc'
-// import heroSvg from '../assets/herobg.jpg';
 
 const ServiceCard =({index, title, icon})=>(
   <Tilt className='xs:w-[250px] w-full'>
